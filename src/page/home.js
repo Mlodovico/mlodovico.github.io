c189f1c6efc71255a3dba26612796dd45bad545a
Murilo Lodovico <37813008+Mlodovico@users.noreply.github.com>
2022-01-17 00:08:34 -0300

    First configure page commit

--- src/page/home.js
+++ src/page/home.js
@@ -1,24 +1,14 @@
-import logo from '../files/logo.svg';
-import './App.css';
+import React from 'react';
+// import logo from "../files/logo.svg";
+import { Container, Text, Wrapper } from './styles';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Wrapper>
+        <Text>Murilo Lodovico</Text>
+      </Wrapper>
+    </Container>
   );
 }
 
---
+++ src/page/styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    align-items: center;
+    justify-content: center;
+    background-color: #808080;
+`;
+
+export const Wrapper = styled.div`
+    height: 90%;
+    width: 90%;
+    align-items: center;
+    justify-content: center;
+    border-radius: 2%;
+    background-color: #DCDCDC;
+`;
+
+export const Text = styled.p`
+    font-size: 20px,
+`;
